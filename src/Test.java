fa8255853363da5780cb2996183279ea4eb9d48d
cup <mai.haoxiang@hzleaper.com>
2026-02-08 21:33:40 +0000

    1

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,23 @@
 public class Test {
+    public static void main(String[] args) {
+
+//        RentalSystem system = new RentalSystem();
+//        Vehicle car = new Car("C1");
+//        system.allVehicles.add(car);
+//
+//        CustomerRecord customer = new CustomerRecord(1);
+//
+//        // 1️⃣ 测试：能否成功租车
+//        boolean hired = system.hireVehicle(customer, "Car");
+//        Assertions.assertTrue(hired);
+//        Assertions.assertTrue(car.isHired());
+//
+//        // 2️⃣ 测试：归还车辆
+//        system.returnVehicle(car.getVehicleID(), customer, 120);
+//
+//        Assertions.assertFalse(car.isHired());
+//        Assertions.assertEquals(120, car.getCurrentMileage());
+//
+//        System.out.println("✅ 所有测试通过！");
+    }
 }
